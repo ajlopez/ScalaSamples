cfb63d8e4888fce9fc730e7517fbf64f50009200
ajlopez <webmaster@ajlopez.com>
2014-05-17 17:45:32 -0300

    Removing parentheses

--- Simple/Timer.scala
+++ Simple/Timer.scala
@@ -2,20 +2,16 @@
 object Timer
 {
   def oncePerSecond(callback: () => Unit): Unit =
-  {
     while (true)
     {
       callback()
       Thread.sleep(1000)
     }
-  }
 
   def timeFlies(): Unit = 
-  { Console.println("Time flies when you're having fun(ctionally)..."); }
+    Console.println("Time flies when you're having fun(ctionally)...")
 
   def main(args: Array[String]): Unit =
-  {
     oncePerSecond(timeFlies)
-  }
 }
 
